4c1a6f698b9553daded11adaaafb5eedfafd3b9e
smarsha8 <shakeithamarshall@yahoo.com>
2023-02-21 21:34:09 -0600

    Add project files.

---
+++ AIRBNB1/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmairbnbreservations
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.btndisplaycost = New System.Windows.Forms.Button()
+        Me.btnclear = New System.Windows.Forms.Button()
+        Me.btnexit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtnights = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblcost = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblprice = New System.Windows.Forms.Label()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.BackgroundImage = Global.AIRBNB1.My.Resources.Resources.airbnb
+        Me.SplitContainer1.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.lblprice)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label6)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label5)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.lblcost)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label4)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.txtnights)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label3)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label2)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label1)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.btnexit)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.btnclear)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.btndisplaycost)
+        Me.SplitContainer1.Size = New System.Drawing.Size(933, 450)
+        Me.SplitContainer1.SplitterDistance = 311
+        Me.SplitContainer1.SplitterWidth = 5
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'btndisplaycost
+        '
+        Me.btndisplaycost.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btndisplaycost.Location = New System.Drawing.Point(72, 350)
+        Me.btndisplaycost.Name = "btndisplaycost"
+        Me.btndisplaycost.Size = New System.Drawing.Size(87, 23)
+        Me.btndisplaycost.TabIndex = 0
+        Me.btndisplaycost.Text = "Display Cost"
+        Me.btndisplaycost.UseVisualStyleBackColor = True
+        '
+        'btnclear
+        '
+        Me.btnclear.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclear.Location = New System.Drawing.Point(269, 350)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(87, 23)
+        Me.btnclear.TabIndex = 1
+        Me.btnclear.Text = "Clear"
+        Me.btnclear.UseVisualStyleBackColor = True
+        '
+        'btnexit
+        '
+        Me.btnexit.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnexit.Location = New System.Drawing.Point(456, 350)
+        Me.btnexit.Name = "btnexit"
+        Me.btnexit.Size = New System.Drawing.Size(87, 23)
+        Me.btnexit.TabIndex = 2
+        Me.btnexit.Text = "Exit"
+        Me.btnexit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Impact", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(281, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(197, 34)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Orlando AirBnB"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(266, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(180, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Only $79.00 per night"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(183, 156)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(153, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Number of Nights:"
+        '
+        'txtnights
+        '
+        Me.txtnights.Location = New System.Drawing.Point(415, 158)
+        Me.txtnights.Name = "txtnights"
+        Me.txtnights.Size = New System.Drawing.Size(32, 20)
+        Me.txtnights.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(180, 217)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(118, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Cost of Stay: "
+        '
+        'lblcost
+        '
+        Me.lblcost.AutoSize = True
+        Me.lblcost.Location = New System.Drawing.Point(453, 224)
+        Me.lblcost.Name = "lblcost"
+        Me.lblcost.Size = New System.Drawing.Size(0, 13)
+        Me.lblcost.TabIndex = 8
+        Me.lblcost.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(405, 227)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Visible = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(403, 233)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(0, 13)
+        Me.Label6.TabIndex = 10
+        '
+        'lblprice
+        '
+        Me.lblprice.AutoSize = True
+        Me.lblprice.Location = New System.Drawing.Point(409, 222)
+        Me.lblprice.Name = "lblprice"
+        Me.lblprice.Size = New System.Drawing.Size(45, 13)
+        Me.lblprice.TabIndex = 11
+        Me.lblprice.Text = "Label7"
+        Me.lblprice.Visible = False
+        '
+        'frmairbnbreservations
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkKhaki
+        Me.ClientSize = New System.Drawing.Size(933, 450)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "frmairbnbreservations"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AirBnB Reservtions"
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnexit As Button
+    Friend WithEvents btnclear As Button
+    Friend WithEvents btndisplaycost As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtnights As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblcost As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblprice As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ AIRBNB1/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class frmairbnbreservations
+    Private Sub SplitContainer1_Panel1_Paint(sender As Object, e As PaintEventArgs) Handles SplitContainer1.Panel1.Paint
+
+    End Sub
+
+    Private Sub txtnights_TextChanged(sender As Object, e As EventArgs) Handles txtnights.TextChanged
+        'Dim strNumberOfDays
+        'Dim strNumberOfDays = txtNumberOfDays.text
+        Dim intNumberofDays As Integer
+        intNumberofDays = 4
+
+    End Sub
+
+    Private Sub SplitContainer1_Panel2_Paint(sender As Object, e As PaintEventArgs) Handles SplitContainer1.Panel2.Paint
+        'decTotalCost As Decimal
+    End Sub
+
+    Private Sub btndisplaycost_Click(sender As Object, e As EventArgs) Handles btndisplaycost.Click
+        lblprice.Visible = True
+        'dim decTotalCost As Decimal = $79.00
+        Dim _decPricePerNight As Decimal = 79D
+        lblprice.Visible = True
+
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles lblprice.Click
+        Dim decTotalCost As Decimal
+
+
+        lblprice.Visible = True
+    End Sub
+
+    Private Sub btnclear_Click(sender As Object, e As EventArgs) Handles btnclear.Click
+        btnclear.
+    End Sub
+
+    Private Sub btnexit_Click(sender As Object, e As EventArgs) Handles btnexit.Click
+        Close()
+    End Sub
+End Class
